1486ae47ea64d41e907035f71c0901d6732c7ff1
Kaizhi Xuan <xuankaizhi@semixchina.com>
2025-08-21 17:28:43 +0800

    37# 多电极结果集的保存和互转

---
+++ Sources/Cores/Core.Models/Models/Common/AODWaveform/PrescanAODWaveformResult.cs
@@ -0,0 +1,18 @@
+﻿using Net.Utilities.Mapper.Interfaces;
+
+namespace Core.Models.Models.Common.AODWaveform;
+
+public class PrescanAODWaveformResult : AODWaveformResult, IAdaptTo<PrescanAODWaveformProfile>, ICloneable<PrescanAODWaveformResult>
+{
+    internal PrescanAODWaveformResult()
+    {
+    }
+
+    public PrescanAODWaveformProfile AdaptTo() => AODWaveformProfileFactory.CreatePrescan(OpticsAODElectrodeEnum, FilePath);
+
+    public PrescanAODWaveformResult Clone() => new()
+    {
+        OpticsAODElectrodeEnum = OpticsAODElectrodeEnum,
+        FilePath = FilePath
+    };
+}
