2c0befbf21feb5870ad2f32b8ff76ae12b7d0024
Kaizhi Xuan <xuankaizhi@semixchina.com>
2025-12-28 17:47:33 +0800

    #104 CIB Illumination Profile 页面OK

---
+++ Sources/Cuga-Calibration-Solution/Views/CIB/IlluminationProfile/Children/Step0View.xaml.cs
@@ -0,0 +1,9 @@
+namespace CugaCalibration.Views.CIB.IlluminationProfile.Children;
+
+public partial class Step0View
+{
+    public Step0View()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Sources/Cuga-Calibration-Solution/Views/CIB/IlluminationProfile/Children/Step4View.xaml.cs
@@ -0,0 +1,9 @@
+namespace CugaCalibration.Views.CIB.IlluminationProfile.Children;
+
+public sealed partial class Step4View
+{
+    public Step4View()
+    {
+        InitializeComponent();
+    }
+}
