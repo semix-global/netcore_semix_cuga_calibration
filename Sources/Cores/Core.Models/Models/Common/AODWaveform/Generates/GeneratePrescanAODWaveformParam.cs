1916fe164be643b32adc5c2f6034f183616dc092
Kaizhi Xuan <xuankaizhi@semixchina.com>
2026-02-06 16:29:12 +0800

    #116 优化

--- Sources/Cores/Core.Models/Models/Common/AODWaveform/Generates/GeneratePrescanAODWaveformParam.cs
+++ Sources/Cores/Core.Models/Models/Common/AODWaveform/Generates/GeneratePrescanAODWaveformParam.cs
@@ -24,6 +24,7 @@ public sealed partial class GeneratePrescanAODWaveformParam :
         ZeroSampleCount = ZeroSampleCount,
         EndpointSampleCount = EndpointSampleCount,
         OffsetConfigurations = [.. ElectrodeConfigurations.Select(t => t.AdaptTo())],
+        P2CompensationCoefficient = P2CompensationCoefficient,
         P3CompensationCoefficient = P3CompensationCoefficient,
         P4CompensationCoefficient = P4CompensationCoefficient,
         P5CompensationCoefficient = P5CompensationCoefficient,
