3f72d107cd5804e1dbed1d600d603bda2c7c3818
Kaizhi Xuan <xuankaizhi@semixchina.com>
2026-02-25 13:37:21 +0800

    #130 优化

--- Sources/Cuga-Calibration-Solution/ViewModels/Ads/AdsYGainsCalibrationViewModel.LoadDepends.cs
+++ Sources/Cuga-Calibration-Solution/ViewModels/Ads/AdsYGainsCalibrationViewModel.LoadDepends.cs
@@ -9,7 +9,6 @@ public sealed partial class AdsYGainsCalibrationViewModel
     {
         if (ApplicationCookie.SysUser.IsAdmin) return true;
 
-        if (ApplicationCookie.SysUser.IsAdmin) return true;
         if (CalibrationStatusService.GetCalibrationDtoIsOKStatus<AdsPressureGainsDto>(out _, out var errorMessage) == false)
         {
             DialogWindowProvider.ShowDialog($"precondition is Failure,Error:{errorMessage}", DialogButtonsEnum.OK, DialogIconEnum.Warning);
