3bdb3faa0f9b30d708e7496b1b9918d318fe9bd8
Kaizhi Xuan <xuankaizhi@semixchina.com>
2026-02-28 09:53:47 +0800

    #130 OK

--- Sources/Cores/Core.Models/Models/Common/DarkField/DarkFieldImageDTO.cs
+++ Sources/Cores/Core.Models/Models/Common/DarkField/DarkFieldImageDTO.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using System.IO;
 using CommunityToolkit.Diagnostics;
 using CommunityToolkit.Mvvm.ComponentModel;
@@ -17,6 +18,8 @@ namespace Core.Models.Models.Common.DarkField;
 
 public partial class DarkFieldRawScanImageDTO :
     ObservableObject,
+    IEquatable<DarkFieldRawScanImageDTO>,
+    IFormattable,
     ICloneable<DarkFieldRawScanImageDTO>
 {
     [ObservableProperty]
@@ -42,6 +45,43 @@ public partial class DarkFieldRawScanImageDTO :
 
         RawImageFilePath = result;
     }
+
+    #region IEquatable、IFormattable
+
+    public bool Equals(DarkFieldRawScanImageDTO? other) => this == other;
+
+    public override bool Equals(object? obj) => obj is DarkFieldRawScanImageDTO other && Equals(other);
+
+    public override int GetHashCode() => HashCode.Combine(CIBInformation, Size, CIBProfileModeEnum, IsForward, RawImageFilePath);
+
+    public override string ToString() => ToString(null);
+
+    public string ToString(string? format, IFormatProvider? formatProvider = null)
+    {
+        formatProvider ??= CultureInfo.CurrentCulture;
+
+        return $"CIB: {CIBInformation}, Size: {Size.ToString(format, formatProvider)}, Mode: {CIBProfileModeEnum}, Forward: {IsForward}";
+    }
+
+    #endregion IEquatable、IFormattable
+
+    #region Operator
+
+    public static bool operator ==(DarkFieldRawScanImageDTO? left, DarkFieldRawScanImageDTO? right) => (left, right) switch
+    {
+        (null, null) => true,
+        (null, _) => false,
+        (_, null) => false,
+        (_, _) => ReferenceEquals(left, right) || (Equals(left.CIBInformation, right.CIBInformation)
+                                                   && Equals(left.Size, right.Size)
+                                                   && Equals(left.CIBProfileModeEnum, right.CIBProfileModeEnum)
+                                                   && Equals(left.IsForward, right.IsForward)
+                                                   && Equals(left.RawImageFilePath, right.RawImageFilePath))
+    };
+
+    public static bool operator !=(DarkFieldRawScanImageDTO? left, DarkFieldRawScanImageDTO? right) => !(left == right);
+
+    #endregion Operator
 
     #region Mapper
 
@@ -72,6 +112,7 @@ public partial class DarkFieldRawScanImageDTO :
 
 public sealed class DarkFieldImageDTO :
     DarkFieldRawScanImageDTO,
+    IEquatable<DarkFieldImageDTO>,
     ICloneable<DarkFieldImageDTO>,
     IAdaptIn<DarkFieldRawScanImageDTO, DarkFieldImageDTO>,
     IDisposable
@@ -79,6 +120,12 @@ public sealed class DarkFieldImageDTO :
     [System.Text.Json.Serialization.JsonIgnore]
     [Newtonsoft.Json.JsonIgnore]
     public required HImage Image { get; init; }
+
+    #region IEquatable、IFormattable
+
+    public bool Equals(DarkFieldImageDTO? other) => this == other;
+
+    #endregion
 
     #region Mapper
 
