97fc88ed2c3f02917198ddb22ca11090b4774207
Kaizhi Xuan <xuankaizhi@semixchina.com>
2026-03-20 16:03:55 +0800

    #146 SC页面ok

---
+++ Sources/Cuga-Calibration-Solution/Views/Optics/SC/Children/Review.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CugaCalibration.Views.Optics.SC.Children;
+
+public sealed partial class Review
+{
+    public Review()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Sources/Cuga-Calibration-Solution/Views/Optics/SC/Children/Step0View.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CugaCalibration.Views.Optics.SC.Children;
+
+public sealed partial class Step0View
+{
+    public Step0View()
+    {
+        InitializeComponent();
+    }
+}
