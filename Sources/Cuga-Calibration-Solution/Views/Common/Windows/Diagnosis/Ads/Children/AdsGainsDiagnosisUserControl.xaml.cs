370f00231af49212de94eb66cacb393b5e4d7345
Kaizhi Xuan <xuankaizhi@semixchina.com>
2026-05-20 17:34:42 +0800

    #170 移除没用的代码

--- Sources/Cuga-Calibration-Solution/Views/Common/Windows/Diagnosis/Ads/Children/AdsGainsDiagnosisUserControl.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace CugaCalibration.Views.Common.Windows.Diagnosis.Ads.Children;
-
-public partial class AdsGainsDiagnosisUserControl
-{
-    public AdsGainsDiagnosisUserControl()
-    {
-        InitializeComponent();
-    }
-}
