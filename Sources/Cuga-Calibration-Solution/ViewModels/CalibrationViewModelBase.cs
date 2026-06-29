4cc5f0868acff538d173b342c052e975a2e3fd8f
Kaizhi Xuan <xuankaizhi@semixchina.com>
2026-06-29 17:32:02 +0800

    #186 format

--- Sources/Cuga-Calibration-Solution/ViewModels/CalibrationViewModelBase.cs
+++ Sources/Cuga-Calibration-Solution/ViewModels/CalibrationViewModelBase.cs
@@ -1,5 +1,4 @@
 using CommunityToolkit.Mvvm.Input;
-using Core.Models.Models.Common.Cookies;
 using Local.SQL.Cache.Providers.Services.Interfaces;
 using Local.SQL.DB.Providers.Models.Entities.Base.Interface;
 using Microsoft.Extensions.Logging;
