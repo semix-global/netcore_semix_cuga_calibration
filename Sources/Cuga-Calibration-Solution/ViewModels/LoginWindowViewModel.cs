1c5e0fb64c44c4495b38227f893115f29de34aee
semix_df70 <semix_df70@semixchina.com>
2026-07-01 14:40:22 +0800

    #189 fa fb dll bug fix

--- Sources/Cuga-Calibration-Solution/ViewModels/LoginWindowViewModel.cs
+++ Sources/Cuga-Calibration-Solution/ViewModels/LoginWindowViewModel.cs
@@ -45,7 +45,7 @@ public partial class LoginWindowViewModel(
 
             UserNames = [.. users.OrderBy(t => t.Id).Select(t => t.UserName)];
 
-            if (users.Count > 0) SysUserDTO = hostEnvironment.IsProduction() ? new SysUserDTO { UserName = users[0].UserName, Password = "vc3ppMjc" } : new SysUserDTO { UserName = users[0].UserName, Password = "666666" };
+            if (users.Count > 0) SysUserDTO = hostEnvironment.IsProduction() ? new SysUserDTO { UserName = users[0].UserName, Password = "GePLqiqO" } : new SysUserDTO { UserName = users[0].UserName, Password = "666666" };
         }
         catch (Exception ex)
         {
