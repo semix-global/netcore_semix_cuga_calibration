0fac741d0a0b803b5896918758d8e1682a053b7c
Kaizhi Xuan <xuankaizhi@semixchina.com>
2026-08-21 19:43:03 +0800

    #203 界面优化

--- Sources/Cuga-Calibration-Solution/Views/Common/Windows/Tools/AODWaveform/ChirpAODWaveformElectrodeOffsetWindow.xaml.cs
+++ Sources/Cuga-Calibration-Solution/Views/Common/Windows/Tools/AODWaveform/ChirpAODWaveformElectrodeOffsetWindow.xaml.cs
@@ -1,5 +1,6 @@
 using CommunityToolkit.Diagnostics;
 using CugaCalibration.ViewModels.Common.Windows.Tools.AODWaveform;
+using Net.Utilities.Models.Geometries;
 using System.Globalization;
 using System.Windows.Data;
 using System.Windows.Markup;
@@ -21,6 +22,20 @@ public sealed class ChirpAODWaveformElectrodeOffsetConvert : MarkupExtension, IV
         if (value is not ChirpAODWaveformElectrodeOffsetItem[] items || parameter is not string format) return ThrowHelper.ThrowNotSupportedException<object>();
 
         return string.Join(", ", items.Select(t => $"{t.Frequency.ToString(format)}: {t.Amplitude.ToString(format)}"));
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) => ThrowHelper.ThrowNotSupportedException<object>();
+
+    public override object ProvideValue(IServiceProvider serviceProvider) => this;
+}
+
+public sealed class ChirpAODWaveformElectrodeOffsetItemToPointsConvert : MarkupExtension, IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value is ChirpAODWaveformElectrodeOffsetItem[] items
+            ? items.Select(t => new Point(t.Frequency, t.Amplitude)).ToArray()
+            : [];
     }
 
     public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) => ThrowHelper.ThrowNotSupportedException<object>();
--- Sources/Cuga-Calibration-Solution/Views/Common/Windows/Tools/AODWaveform/PrescanAODWaveformElectrodeOffsetWindow.xaml.cs
+++ Sources/Cuga-Calibration-Solution/Views/Common/Windows/Tools/AODWaveform/PrescanAODWaveformElectrodeOffsetWindow.xaml.cs
@@ -1,5 +1,6 @@
 using CommunityToolkit.Diagnostics;
 using CugaCalibration.ViewModels.Common.Windows.Tools.AODWaveform;
+using Net.Utilities.Models.Geometries;
 using System.Globalization;
 using System.Windows.Data;
 using System.Windows.Markup;
@@ -21,6 +22,20 @@ public sealed class PrescanAODWaveformElectrodeOffsetConvert : MarkupExtension, 
         if (value is not PrescanAODWaveformElectrodeOffsetItem[] items || parameter is not string format) return ThrowHelper.ThrowNotSupportedException<object>();
 
         return string.Join(", ", items.Select(t => $"{t.Frequency.ToString(format)}: {t.Amplitude.ToString(format)}"));
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) => ThrowHelper.ThrowNotSupportedException<object>();
+
+    public override object ProvideValue(IServiceProvider serviceProvider) => this;
+}
+
+public sealed class PrescanAODWaveformElectrodeOffsetItemToPointsConvert : MarkupExtension, IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value is PrescanAODWaveformElectrodeOffsetItem[] items
+            ? items.Select(t => new Point(t.Frequency, t.Amplitude)).ToArray()
+            : [];
     }
 
     public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) => ThrowHelper.ThrowNotSupportedException<object>();
