41011ead6d56cc9bf52826e3183da6c8fd80c672
Kaizhi Xuan <xuankaizhi@semixchina.com>
2026-09-01 20:00:11 +0800

    #196 2.9.5-preview6-xuan

---
+++ Sources/Cores/Core.Models/Models/Fourier/CameraAlignment/PupilCameraAlignmentCache.cs
@@ -0,0 +1,9 @@
+using Local.SQL.Cache.Providers.Bases;
+
+namespace Core.Models.Models.Fourier.CameraAlignment;
+
+[CacheVersion("2.0.0")]
+public sealed partial class PupilCameraAlignmentCache : CalibrationCacheBase<PupilCameraAlignmentCache>
+{
+
+}
