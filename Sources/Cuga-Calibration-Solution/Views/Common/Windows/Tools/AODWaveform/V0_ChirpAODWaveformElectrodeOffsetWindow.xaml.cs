99dbee2b878665c84844a81efc4590f02a7b9e80
Kaizhi Xuan <xuankaizhi@semixchina.com>
2026-09-04 14:43:04 +0800

    #214 移除没用的

--- Sources/Cuga-Calibration-Solution/Views/Common/Windows/Tools/AODWaveform/V0_ChirpAODWaveformElectrodeOffsetWindow.xaml.cs
+++ Sources/Cuga-Calibration-Solution/Views/Common/Windows/Tools/AODWaveform/V0_ChirpAODWaveformElectrodeOffsetWindow.xaml.cs
@@ -1,10 +1,3 @@
-using CommunityToolkit.Diagnostics;
-using CugaCalibration.ViewModels.Common.Windows.Tools.AODWaveform;
-using System.Globalization;
-using System.Windows;
-using System.Windows.Data;
-using System.Windows.Markup;
-
 namespace CugaCalibration.Views.Common.Windows.Tools.AODWaveform;
 
 public partial class V0ChirpAODWaveformElectrodeOffsetWindow
@@ -13,21 +6,4 @@ public partial class V0ChirpAODWaveformElectrodeOffsetWindow
     {
         InitializeComponent();
     }
-}
-
-public sealed class V0ChirpAODWaveformElectrodeOffsetConvert : MarkupExtension, IValueConverter
-{
-    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
-    {
-        if (value is not IEnumerable<V0ChirpAODWaveformElectrodeOffsetItem> items) return ThrowHelper.ThrowNotSupportedException<object>();
-
-        var item = items.FirstOrDefault();
-
-        return item is null ? DependencyProperty.UnsetValue : $"{item.Frequency}MHz";
-    }
-
-    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
-        => ThrowHelper.ThrowNotSupportedException<object>();
-
-    public override object ProvideValue(IServiceProvider serviceProvider) => this;
 }
