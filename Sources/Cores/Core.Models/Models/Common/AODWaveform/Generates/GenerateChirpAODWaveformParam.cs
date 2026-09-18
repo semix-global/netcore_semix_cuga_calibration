b3fe1a455f9320ac12d38edf6dad6a552c0d4906
Kaizhi Xuan <xuankaizhi@semixchina.com>
2026-09-18 20:40:41 +0800

    #228 波形还原cuga应用的json bug fix

--- Sources/Cores/Core.Models/Models/Common/AODWaveform/Generates/GenerateChirpAODWaveformParam.cs
+++ Sources/Cores/Core.Models/Models/Common/AODWaveform/Generates/GenerateChirpAODWaveformParam.cs
@@ -36,7 +36,7 @@ public sealed partial class GenerateChirpAODWaveformParam :
         FileNameSuffix = $"{ProductivityInformation.OpticsIlluminationModeEnum}_{ProductivityInformation.AdaptTo().Mag}",
         ZeroSampleCount = ZeroSampleCount,
         EndpointSampleCount = EndpointSampleCount,
-        ElectrodeConfigurations = [.. ElectrodeConfigurations.Select(t => t.AdaptTo())],
+        OffsetConfigurations = [.. ElectrodeConfigurations.Select(t => t.AdaptTo())],
         SlopeConfigurations = [.. SlopeConfigurations.Select(t => t.AdaptTo())]
     };
 
